2516e7e1543faf75a9611085b9ca8a494c330174
GuilhermeAlex <guilhermealex01@gmail.com>
2021-01-22 20:51:51 -0300

    rotas de entrar e cadastrar criadas

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private athService: AuthService,
+    private router: Router
+  ) { }
+
+
+  ngOnInit()  {
+    window.scroll(0,0)
+
   }
+    confirmSenha(Event: any) {
+      this.confirmarSenha = Event.target.value
+      
+  }
+    tipoUser(Event: any){
+      this.tipoUsuario = Event.target.value
+    }
+    cadastrar(){
+      this.user.tipo = this.tipoUsuario
+
+      if(this.user.senha !=  this.confirmarSenha){
+        alert ('A senhas estão incorretas.')
+      }else {
+        this.athService.cadastrar(this.user).subscribe((resp: User) => {
+          this.user = resp 
+          this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+      }
+
+    }
+
 
 }
