b51dc3d1b8bc3be6d022aa211e6f70613644256c
GuilhermeAlex <guilhermealex01@gmail.com>
2021-01-28 15:26:31 -0300

    Implementação do get e do post de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+    private http: HttpClient /*Métodos http */ 
+  ) { }
+
+  /* Objeto Token que recebe headers e cria um novo HttpHeaders*/
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token ) /*SET é colocar */
+  }
+  /*operações do crud GETALL*/
+  /*Arrey de Objetos temas [] */
+  getAllTema(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
+  }
+  /*operação do crud Post */ 
+  /*Esse tema é unicio por isso não tem  Arrey[] */
+  postTema(tema: Tema): Observable<Tema>{ /*Esse Observable é de tema unico */
+    return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
+  }
+}
